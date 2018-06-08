6a8034428f3eaec4e2ad23915267360e45b6702d
mhudnell <maxhudnell@gmail.com>
2018-06-08 13:49:19 -0400

    forced one test to finish before starting the next to make console logs be in correct order, added testAll func

--- test_suite.js
+++ test_suite.js
@@ -3,6 +3,7 @@ const fs = require('fs'),
       PNG = require('pngjs').PNG,
       pixelmatch = require('pixelmatch'),
       puppeteer = require('puppeteer');
+      // sharedComponents = require('../client/src/testComponentsNames.js');
 
 const defaultBrowserOpts = {
   x: 0,
@@ -13,6 +14,7 @@ const defaultBrowserOpts = {
 
 module.exports = {
   testOne: async function(name, browserOpts=defaultBrowserOpts) {
+    console.log("Testing " + name + " . . ...");
     const browser = await puppeteer.launch(browserOpts);
     const page = await browser.newPage();
 
@@ -23,23 +25,44 @@ module.exports = {
       await page.screenshot({path: 'screenshots/expected/' + name + '.png'});
     }
     await page.screenshot({path: 'screenshots/actual/' + name + '.png'});
+    await browser.close();
 
+    // load pngs into variables
+    var imgExpected = fs.createReadStream('screenshots/expected/' + name + '.png').pipe(new PNG()),
+        imgActual = fs.createReadStream('screenshots/actual/' + name + '.png').pipe(new PNG());
 
-    var doneReading = function() {
-      if (++filesRead < 2) return;
+    // create promises to tell us when pngs have loaded into imgExpected/imgActual
+    var imgEpromise = new Promise((resolve, reject) => {
+      imgExpected.on('parsed', () => resolve('successfully parsed'));
+      imgExpected.on('error', () => reject('nogood'));
+    }).catch(error => {console.log('caught', error.message)});
+    var imgApromise = new Promise((resolve, reject) => {
+      imgActual.on('parsed', () => resolve('successfully parsed'));
+      imgActual.on('error', () => reject('nogood'));
+    }).catch(error => {console.log('caught', error.message)});
+
+    var doneReading = async function() {
+      await imgEpromise;
+      await imgApromise;
+
       var diff = new PNG({width: imgExpected.width, height: imgExpected.height});
 
       var numMismatch = pixelmatch(imgExpected.data, imgActual.data, diff.data, imgExpected.width, imgExpected.height, {threshold: 0.1});
+      numMismatch ? console.log("TEST \x1b[31m%s\x1b[0m", "FAILED") : console.log("TEST \x1b[32m%s\x1b[0m", "PASSED");
       console.log("numMismatch: " + numMismatch + " pixels out of " + (browserOpts.width * browserOpts.height));
-      console.log("percMismatch: " + numMismatch * 100 / (browserOpts.width * browserOpts.height) + " %");
+      console.log("percMismatch: " + numMismatch * 100 / (browserOpts.width * browserOpts.height) + " %\n");
 
       diff.pack().pipe(fs.createWriteStream('screenshots/diff/' + name + '.png'));
+      return numMismatch;
     };
 
-    var filesRead = 0;
-        imgExpected = fs.createReadStream('screenshots/expected/' + name + '.png').pipe(new PNG()).on('parsed', doneReading),
-        imgActual = fs.createReadStream('screenshots/actual/' + name + '.png').pipe(new PNG()).on('parsed', doneReading);
+    var numMismatch = await doneReading();
 
-    await browser.close();
+    return numMismatch;
+  },
+
+  testAll: async function() {
+    await module.exports.testOne('Accordion');
+    await module.exports.testOne('AccordionGroup');
   }
 }

4f32a13ef2385154eb07a4d824cde64559349ff1
mhudnell <maxhudnell@gmail.com>
2018-06-12 15:14:51 -0400

    adding more tests

--- tests.js
+++ tests.js
@@ -2,10 +2,14 @@
 /*
 *   write component tests here, can specify screenshot size
 *   key must match React component name in 'client/src/testComponents.js` (this name is used in the url we navigate to)
+*   clip must be of format {x:?, y:?, width:?, height:?}
 */
 const tests = {
   Accordion: {},
   AccordionGroup: { clip: {x:0, y:0, width:800, height:750} },
+  Alert: {clip: {x:0, y:0, width:800, height:100}},
+  Button: {clip: {x:0, y:0, width:500, height:100}},
+  Card: { clip: {x:0, y:0, width:800, height:300} },
 }
 
 module.exports = tests;

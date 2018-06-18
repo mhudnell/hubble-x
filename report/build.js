0ddf2cc4b643dbbba75e6cb9e05b823cf6fedda1
mhudnell <maxhudnell@gmail.com>
2018-06-18 15:29:55 -0400

    add visual feedback for pass/fail in html report

--- report/build.js
+++ report/build.js
@@ -2,44 +2,41 @@ const { promisify } = require('util');
 const ejsRenderFile = promisify(require('ejs').renderFile);
 const fs = require('fs');
 const path = require('path');
+const xml2js = require('xml2js');
+const xmlParseString = promisify(require('xml2js').parseString);
+
+//dev
+// const util = require('util');
 
 var files = fs.readdirSync('../screenshots/expected/');
 var names = files.map(file => path.basename(file, '.png'));
 
-ejsRenderFile('src/pages/index.ejs', {}).then((pageContent) => {
-  return ejsRenderFile('src/layouts/layout.ejs', {body: pageContent, names: names});
-})
-.then((layoutContent) => {
-  fs.writeFile('dist/index.html', layoutContent, (err) => {  
-    if (err) throw err;
+let xmlString = fs.readFileSync(__dirname + '/../screenshots/report.xml');
 
-    // success case, the file was saved
-    console.log('html report generated!');
-  });
-})
 
-names.forEach((name) => {
-  ejsRenderFile('src/pages/test.ejs', {name: name}).then((pageContent) => {
-    return ejsRenderFile('src/layouts/layout.ejs', {body: pageContent, names: names});
+xmlParseString(xmlString).then((xml) => {
+  let report = xml['vizregResults'];
+  // console.log(util.inspect(xml, false, null));
+
+  // create index.html
+  ejsRenderFile('src/pages/index.ejs', {}).then((pageContent) => {
+    return ejsRenderFile('src/layouts/layout.ejs', {body: pageContent, names: names, report: report});
   })
   .then((layoutContent) => {
-    fs.writeFile('dist/'+name+'.html', layoutContent, (err) => {  
+    fs.writeFile('dist/index.html', layoutContent, (err) => {  
       if (err) throw err;
-
-      // success case, the file was saved
-      console.log('html report generated!');
     });
   })
+
+  // create html pages for all tests
+  names.forEach((name) => {
+    ejsRenderFile('src/pages/test.ejs', {name: name}).then((pageContent) => {
+      return ejsRenderFile('src/layouts/layout.ejs', {body: pageContent, names: names, report: report});
+    })
+    .then((layoutContent) => {
+      fs.writeFile('dist/'+name+'.html', layoutContent, (err) => {  
+        if (err) throw err;
+      });
+    })
+  })
 })
-
-
-
-
-// ejs.renderFile('src/index.ejs', {}).then(function(html) {
-//   fs.writeFile('index.html', html, (err) => {  
-//     if (err) throw err;
-
-//     // success case, the file was saved
-//     console.log('html report generated!');
-//   });
-// })
